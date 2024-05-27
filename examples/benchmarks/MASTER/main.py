3b09edee80a00cee08466d14a7b85e34e2e29b93
LeonTing1010 <zhongchang.ding@gmail.com>
2024-05-27 15:23:51 +0800

    3-2-1

--- examples/benchmarks/MASTER/main.py
+++ examples/benchmarks/MASTER/main.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
         ]
     }
 
-    for seed in range(100, 101):
+    for seed in range(130, 133):
         print("------------------------")
         print(f"seed: {seed}")
 
